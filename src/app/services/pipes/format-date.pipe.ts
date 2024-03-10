661fb757931cbdecd34d64221ec6912ad0f6bd8c
lufecrx <luizfelipecrx@gmail.com>
2024-03-10 16:25:07 -0300

    corrigir a formatação das datas

--- src/app/services/pipes/format-date.pipe.ts
+++ src/app/services/pipes/format-date.pipe.ts
@@ -6,9 +6,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatDatePipe implements PipeTransform {
 
   transform(value: any): string  {
-    // Format date aaaa-mm-dd to dd/mm/aaaa
-    const date = new Date(value);
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    // Verifica se o valor é uma string
+    if (typeof value !== 'string') {
+      return 'Invalid Date';
+    }
+
+    // Divide a string da data nos elementos separados
+    const dateElements = value.split('-');
+
+    // Verifica se a string da data está no formato esperado
+    if (dateElements.length !== 3) {
+      return 'Invalid Date Format';
+    }
+
+    // Extrai os elementos da data
+    const year = dateElements[0];
+    const month = dateElements[1];
+    const day = dateElements[2];
+
+    // Formata a data para dd/mm/aaaa
+    return `${day}/${month}/${year}`;
   }
 
 }
